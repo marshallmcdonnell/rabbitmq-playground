13bd02e975a33358bd39d2884064c3428fc234aa
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-04-03 16:12:57 -0400

    Added gitignore and filling out the python publisher service

---
+++ container-setup-basic/publisher/services/user_event_handler.py
@@ -0,0 +1,27 @@
+import pika
+import json
+
+def emit_user_profile_update(user_id, new_data):
+    params = pika.ConnectionParameters(host='rabbitmq-server')
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+
+    exchange_name = "user_updates"
+    routing_key = "user.profile.update"
+
+    channel.exchange_declare(exchange=exchange_name, exchange_type='topic', durable=True)
+    
+    new_data['id'] = user_id
+
+    channel.basic_publish(
+        exchange=exchange_name,
+        routing_key=routing_key,
+        body=json.dumps(new_data),
+        properties=pika.BasicProperties(delivery_mode=2))
+
+    msg = "{routing_key} send to exchange {exchange_name} with data: {data}"
+    print(msg.format(
+        routing_key=routing_key,
+        exchange_name=exchange_name,
+        data=new_data)
+    connection.close()
